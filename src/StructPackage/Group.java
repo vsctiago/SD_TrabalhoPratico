a9e9ae276002779202d163f08c15bdf0b27b3e85
vsctiago <vsctiago@yahoo.com>
2017-01-03 21:57:34 +0000

    asd

--- src/StructPackage/Group.java
+++ src/StructPackage/Group.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package StructPackage;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Tiago Fernandes
- */
 public class Group {
     
     private String name;
